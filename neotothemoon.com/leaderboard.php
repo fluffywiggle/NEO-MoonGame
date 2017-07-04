2e31f14a38f307b6a2ea0a207a7f6e5e38dbe979
m3ndi3 <m3ndi3@gmail.com>
2017-07-04 17:11:51 -0400

    Add files via upload

--- neotothemoon.com/leaderboard.php
+++ neotothemoon.com/leaderboard.php
@@ -5,53 +5,13 @@
 <section id="part-1">
 
   <div class="content">
-    <div id="leaderboard">
+    <?php 	$numAmount = 20;
 
 
+    
+    	include 'pages/leaderboardOutput.php';
 
-    <h2 style="font-size:35px; font-weight:bold; text-align:center;  margin-top:0px; margin-bottom:20px;">Leaderboard</h2><center>
-
-
-
-<p style="font-size:20px;">Weekly leaderboard ends every Friday at 10AM EDT.</p>
-<div id="clockdiv">
-<div>
-<span class="days"></span>
-<div class="smalltext">Days</div>
-</div>
-<div>
-<span class="hours"></span>
-<div class="smalltext">Hours</div>
-</div>
-<div>
-<span class="minutes"></span>
-<div class="smalltext">Minutes</div>
-</div>
-<div>
-<span class="seconds"></span>
-<div class="smalltext">Seconds</div>
-</div>
-</div>
-
-      <?php
-
-      include 'pages/db.php';
-$numAmount = 10;
-
-include 'pages/leaderboardOutput.php';
-
-?>
-
-
-
-
-
-
-
-
-<span class="smalltext"> *If there are multiple users with same score, they will share that tier's reward</span>
-
- <div style="clear:both"></div></div>
+    	?>
   </div>
 </section>
 
