c49464fe77ba2e68de1869db45ee7513e3f5ca14
m3ndi3 <m3ndi3@gmail.com>
2017-07-04 17:11:36 -0400

    Add files via upload

--- neotothemoon.com/pages/leaderboardOutput.php
+++ neotothemoon.com/pages/leaderboardOutput.php
@@ -1,4 +1,34 @@
+<div id="leaderboard">
+
+
+
+<h2 style="font-size:35px; font-weight:bold; text-align:center;  margin-top:0px; margin-bottom:20px;">Leaderboard</h2><center>
+
+
+
+<p style="font-size:20px;">Weekly leaderboard ends every Friday at 10AM EDT.</p>
+<div id="clockdiv">
+<div>
+<span class="days"></span>
+<div class="smalltext">Days</div>
+</div>
+<div>
+<span class="hours"></span>
+<div class="smalltext">Hours</div>
+</div>
+<div>
+<span class="minutes"></span>
+<div class="smalltext">Minutes</div>
+</div>
+<div>
+<span class="seconds"></span>
+<div class="smalltext">Seconds</div>
+</div>
+</div>
+
 <?php // Create connection
+
+  include 'pages/db.php';
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
@@ -59,3 +89,6 @@ else {
 }
 $conn->close();
 ?>
+
+<span class="smalltext"> *If there are multiple users with same score, they will share that tier's reward</span>
+		 <div style="clear:both"></div></div>
